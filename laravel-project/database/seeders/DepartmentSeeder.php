0f3358b57d9ba0d4626e24a1d80872f9299f9b90
Finsa Nurpandi <finsaworks@gmail.com>
2024-11-06 10:26:19 +0700

    laravel seeder

---
+++ laravel-project/database/seeders/DepartmentSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //id, code, name, alias
+        DB::table('departments')
+            ->insert([
+                [
+                    'id' => '1',
+                    'code' => '22201',
+                    'name' => 'Teknik Sipil',
+                    'alias' => 'SI',
+                ],
+                [
+                    'id' => '2',
+                    'code' => '26201',
+                    'name' => 'Teknik Industri',
+                    'alias' => 'TI',
+                ],
+                [
+                    'id' => '3',
+                    'code' => '55201',
+                    'name' => 'Teknik Informatika',
+                    'alias' => 'IF',
+                ]
+            ]);
+    }
+}
